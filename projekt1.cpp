4d075d9d89059f8c7506d71d820e35b92e577799
Filip Brytan <f.brytan@o2.pl>
2021-05-02 21:20:37 +0200

    open

--- projekt1.cpp
+++ projekt1.cpp
@@ -1,62 +1,40 @@
 // projekt1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+#define _USE_MATH_DEFINES
 #include<iostream>
 #include <stack>
-using namespace std;
-using namespace System;
-using namespace System::Drawing;
+#include <math.h>
+#include <vector>
 
-void histogramRGB( Bitmap^ obraz, double odch, std::string nazwa_obrazu );
-void histogramMono( Bitmap^ obraz, double odch, std::string nazwa_obrazu );
-
-void ordFilt2RGB( Bitmap^ obraz, double mask, double num_porz, std::string nazwa_obrazu );
-void ordFilt2Mono( Bitmap^ obraz, double mask, double num_porz, std::string nazwa_obrazu );
-
-void openMono( Bitmap^ obraz, double length, double dgr, std::string nazwa_obrazu );
-void openLogic( Bitmap^ obraz, double length, double dgr, std::string nazwa_obrazu );
-
-void holeFillLogic( Bitmap^ obraz, std::string nazwa_obrazu );
-
-struct punkt {
+struct punkt
+{
   int x;
   int y;
 };
-typedef struct punkt punkt;
 
+using namespace std;
+using namespace System;
+using namespace System::Drawing;
+//wyrownanie histogramu
+void histogramRGB( Bitmap^ obraz, double odch, std::string nazwa_obrazu );
+void histogramMono( Bitmap^ obraz, double odch, std::string nazwa_obrazu );
 
-Bitmap^ floodFill( Bitmap^ obraz, Color old, Color fill )
-{
-  Color Px;
-  int h = obraz->Height;
-  int w = obraz->Width;
-  punkt start;
-  start.x = h / 2;
-  start.y = w / 2;
-  stack<punkt> stos;
-  stos.push( start );
-  while ( stos.size() > 0 )
-  {
-    punkt p = stos.top();
-    stos.pop();
-    if ( p.y < 0 || p.y > w - 1 || p.x < 0 || p.x > h - 1 )
-      continue;
-    Color val = obraz->GetPixel( p.y, p.y );
-    if ( val.R == old.R )
-    {
-      obraz->SetPixel( p.y, p.x, fill );
-      punkt p0{ p.x + 1, p.y };
-      stos.push(p0);
-      punkt p1{ p.x - 1, p.y };
-      stos.push( p1 );
-      punkt p2{ p.x , p.y + 1 };
-      stos.push( p2 );
-      punkt p3{ p.x, p.y- 1 };
-      stos.push( p3 );
+//ordfilt
+void ordFilt2RGB( Bitmap^ obraz, double mask, double num_porz, std::string nazwa_obrazu );
+void ordFilt2Mono( Bitmap^ obraz, double mask, double num_porz, std::string nazwa_obrazu );
 
-    }
-  }
-  return obraz;
-}
+//otwarcie elementem linjnym
+void open( Bitmap^& obraz, int length, int dgr, std::string nazwa_obrazu );
+//wypelnianie dziur
+void holeFillLogic( Bitmap^ obraz, std::string nazwa_obrazu );
+
+//rysowanie linii pod zadanym k¹tem
+void BresenhamLine( const int x1, const int y1, const int x2, const int y2, vector< vector<bool> >& SE );
+//void CenterLine( vector<punkt>& SE );
+void erosion( Bitmap^& output, Bitmap^& obraz, vector< vector<bool> >& SE, int SE_height, int SE_width );
+void dilation( Bitmap^& output, Bitmap^& obraz, vector< vector<bool> >& SE, int SE_height, int SE_width );
+
+void menu( int choice, Bitmap^& obraz, std::string nazwa_obrazu );
 int main()
 {
     std::cout << "Podaj nazwe obrazu!\n";
@@ -65,9 +43,278 @@ int main()
     Bitmap^ obraz;
     String^ newSystemString = gcnew String( nazwa_obrazu.c_str() );
     obraz = gcnew Bitmap( newSystemString, true );
-    holeFillLogic( obraz, nazwa_obrazu );
+
+    int choice;
+    cout << "Co wykonac, 1) Wypelnianie dziur \n2)Otwarcie elementem linijnym" << endl;
+    cin >> choice;
+    menu( choice, obraz, nazwa_obrazu );
 }
 
+void menu( int choice, Bitmap^& obraz, std::string nazwa_obrazu )
+{
+    switch ( choice )
+    {
+    case 1:      
+      cout << "Wybrano wypelnianie dziur\n";
+      holeFillLogic( obraz, nazwa_obrazu );
+
+      break;
+
+    case 2:   
+      cout << "Wybrano otwarcie elementem linijnym.\n";
+      cout << "Podaj dlugosc odcinka\n";
+      int dlug;
+      cin >> dlug;
+      cout << "Podaj kat w stopniach\n";
+      int kat;
+      cin >> kat;
+      open( obraz, dlug, kat, nazwa_obrazu );
+      break;
+
+    case 3:   
+      cout << "Ordfilt2";
+      break;
+
+    default:  
+      cout << "Zla opcja";
+    }
+}
+void open( Bitmap^& obraz, int length, int dgr, std::string nazwa_obrazu )
+{
+  dgr = dgr % 360;
+  double radian = dgr * double( M_PI ) / 180;
+  int ptk_1 = (cos( radian ) * length);
+  int ptk_2 = (sin( radian ) * length);
+
+  int SE_width = static_cast< int >( ptk_1 ); 
+  int SE_height = static_cast< int >( ptk_2 );
+  if ( SE_width % 2 == 0 ) ++SE_width;
+  if ( SE_height % 2 == 0 ) ++SE_height;
+  int centralPointX = ( SE_width - 1 ) / 2;
+  int centralPointY = ( SE_height - 1 ) / 2;
+  vector<vector<bool>> vect;
+  vect.resize( SE_height );
+  for ( int i = 0; i < SE_height; ++i )
+    vect[ i ].resize( SE_width );
+  for ( int i = 0; i < SE_height; i++ )
+  {
+    for ( int j = 0; j < SE_width; j++ )
+    {
+      cout << vect[ i ][ j ] << "\t";
+
+    }
+    cout << "\n";
+  }
+  int x0 = SE_height - 1;
+  int y0 = 0;
+  int x1 = 0;
+  int y1 = ( SE_width - 1 );
+  BresenhamLine( x0, y0, x1, y1, vect );
+
+  //vect[ x0 ][ y0 ] = 1;
+  //vect[ x1 ][ y1 ] = 1;
+
+
+  //int dx = abs( x1 - x0 );
+  //int sx = x0 < x1 ? 1 : -1;
+  //int dy = -abs( y1 - y0 );
+  //int sy = y0 < y1 ? 1 : -1;
+  //int err = dx + dy;
+  //while ( true ) {
+
+  //  if ( x0 >= x1 && y0 >= y1 ) break;
+  //  vect[ x0 ][ y0 ] = 1;
+
+  //  int e2 = 2 * err;
+  //  if ( e2 >= dy ) {
+  //    err += dy;
+  //    x0 += sx;
+  //  }
+  //  if ( e2 <= dx ) {
+
+  //    err += dx;
+  //    y0 += sy;
+  //  }
+
+  //}
+  cout << " \n\n LINIA \n\n";
+  for ( int i = 0; i < SE_height; i++ )
+  {
+    for ( int j = 0; j < SE_width; j++ )
+    {
+      cout << vect[ i ][ j ] << "\t";
+
+    }
+    cout << "\n";
+  }
+  Bitmap^ output  = gcnew Bitmap( obraz->Width, obraz->Height );
+  Bitmap^ eroded = gcnew Bitmap( obraz->Width, obraz->Height );
+  Bitmap^ dilated = gcnew Bitmap( obraz->Width, obraz->Height );
+  dilation( dilated, obraz, vect, SE_height, SE_width );
+  dilated->Save( "dilation.png" );
+  erosion( eroded, obraz, vect, SE_height, SE_width );
+  eroded->Save( "erosion.png" );
+  dilation(output, eroded, vect, SE_height, SE_width );
+  output->Save( "open.png" );
+}
+//void CenterLine( vector<punkt>& SE )
+//{
+//  int srodek = SE.size() / 2;
+//  cout << srodek << " " << SE.size() << endl;
+//  vector<punkt> temp = SE;
+//  temp[ srodek ] = { 0,0 };
+//  for ( int i = srodek; i > 0; i-- )
+//  {
+//    punkt dyst;
+//    dyst.x = SE[ i - 1 ].x;
+//    dyst.y = SE[ i - 1 ].y;
+//    temp[ i - 1 ].x = SE[ i - 1 ].x - SE[ srodek ].x;
+//    temp[ i - 1 ].y = SE[ i - 1 ].y - SE[ srodek ].y;
+//  }
+//  for ( int i = srodek; i < SE.size()-1; i++ )
+//  {
+//    punkt dyst;
+//    dyst.x = SE[ i + 1 ].x;
+//    dyst.y = SE[ i + 1 ].y;
+//    temp[ i + 1 ].x = SE[ i + 1 ].x - SE[ srodek ].x;
+//    temp[ i + 1 ].y = SE[ i + 1 ].y - SE[ srodek ].y;
+//  }
+//
+//  SE = temp;
+//}
+void erosion(Bitmap^& output, Bitmap^& obraz, vector< vector<bool> >& SE, int SE_height, int SE_width )
+{
+  int wys = obraz->Height;
+  int szer = obraz->Width;
+  for ( int i = 0; i < szer; i++ )
+  {
+    for ( int j = 0; j < wys; j++ )
+    {
+      Color Px = obraz->GetPixel( j, i );
+      int min{ Px.R };
+      for ( int kx=0; kx<SE_height; kx++  )
+      {
+        for ( int kz = 0; kz < SE_width; kz++ )
+        {
+          if(SE[kx][kz])
+            if ( ( j + kz >= 0 && j +kz < szer ) && ( i + kx >= 0 && i + kx < wys ) )
+            {
+              Color Px = obraz->GetPixel( j + kz, i + kx );
+              if ( Px.R < min )
+                min = Px.R;
+            }
+        }
+      }
+      output->SetPixel( j, i, Color::FromArgb( min, min, min ) );
+    }
+  }
+}
+void dilation( Bitmap^& output, Bitmap^& obraz, vector< vector<bool> >& SE, int SE_height, int SE_width )
+{
+  int wys = obraz->Height;
+  int szer = obraz->Width;
+  for ( int i = 0; i < szer; i++ )
+  {
+    for ( int j = 0; j < wys; j++ )
+    {
+      Color Px = obraz->GetPixel( j, i );
+      int max{ Px.R };
+      for ( int kx = 0; kx < SE_height; kx++ )
+      {
+        for ( int kz = 0; kz < SE_width; kz++ )
+        {
+          if ( SE[ kx ][ kz ] )
+            if ( ( j + kx >= 0 && j + kx < szer ) && ( i + kz >= 0 && i + kz < wys ) )
+            {
+              Color Px = obraz->GetPixel( j + kx, i + kz );
+              if ( Px.R > max )
+                max = Px.R;
+            }
+        }
+      }
+      output->SetPixel( j, i, Color::FromArgb( max, max, max ) );
+    }
+  }
+}
+void BresenhamLine( const int x1, const int y1, const int x2, const int y2, vector< vector<bool> >& SE )
+{
+  // zmienne pomocnicze
+  int d, dx, dy, ai, bi, xi, yi;
+  int x = x1, y = y1;
+  // ustalenie kierunku rysowania
+  if ( x1 < x2 )
+  {
+    xi = 1;
+    dx = x2 - x1;
+  }
+  else
+  {
+    xi = -1;
+    dx = x1 - x2;
+  }
+  // ustalenie kierunku rysowania
+  if ( y1 < y2 )
+  {
+    yi = 1;
+    dy = y2 - y1;
+  }
+  else
+  {
+    yi = -1;
+    dy = y1 - y2;
+  }
+  // pierwszy piksel
+  SE[ x ][ y ] = true;
+  //glVertex2i( x, y );
+  // oœ wiod¹ca OX
+  if ( dx > dy )
+  {
+    ai = ( dy - dx ) * 2;
+    bi = dy * 2;
+    d = bi - dx;
+    // pêtla po kolejnych x
+    while ( x != x2 )
+    {
+      // test wspó³czynnika
+      if ( d >= 0 )
+      {
+        x += xi;
+        y += yi;
+        d += ai;
+      }
+      else
+      {
+        d += bi;
+        x += xi;
+      }
+      SE[ x ][ y ] = true;
+    }
+  }
+  // oœ wiod¹ca OY
+  else
+  {
+    ai = ( dx - dy ) * 2;
+    bi = dx * 2;
+    d = bi - dy;
+    // pêtla po kolejnych y
+    while ( y != y2 )
+    {
+      // test wspó³czynnika
+      if ( d >= 0 )
+      {
+        x += xi;
+        y += yi;
+        d += ai;
+      }
+      else
+      {
+        d += bi;
+        y += yi;
+      }
+      SE[ x ][ y ] = true;
+    }
+  }
+}
 void histogramRGB( Bitmap^ obraz, double odch, std::string nazwa_obrazu ) {
 
 }
@@ -78,12 +325,6 @@ void ordFilt2RGB( Bitmap^ obraz, double mask, double num_porz, std::string nazwa
 {
 }
 void ordFilt2Mono( Bitmap^ obraz, double mask, double num_porz, std::string nazwa_obrazu )
-{
-}
-void openMono( Bitmap^ obraz, double length, double dgr, std::string nazwa_obrazu )
-{
-}
-void openLogic( Bitmap^ obraz, double length, double dgr, std::string nazwa_obrazu )
 {
 }
 void holeFillLogic( Bitmap^ obraz, std::string nazwa_obrazu )
@@ -196,13 +437,4 @@ void holeFillLogic( Bitmap^ obraz, std::string nazwa_obrazu )
   output->Save( newSystemString );
 
 }
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
